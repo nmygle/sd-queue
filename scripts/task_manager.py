0231197130fd4f36cfe43bd2ed98b409b5301658
nao <nao@naonoMacBook-Pro.local>
2023-10-05 07:38:35 +0900

    update

--- scripts/task_manager.py
+++ scripts/task_manager.py
@@ -10,14 +10,16 @@ class TaskManager:
         self.tasks_db = OrderedDict()
         self.tasks_queue = Queue()
         self.stop_worker = threading.Event()
+        self.lock = threading.Lock()
         self._worker_start()
 
     def _update_status(self, task_id, status, result=None):
         """statusの状態を変更"""
-        if task_id in self.tasks_db:
-            self.tasks_db[task_id]["status"] = status
-            if result is not None:
-                self.tasks_db[task_id]["result"] = result
+        with self.lock:
+            if task_id in self.tasks_db:
+                self.tasks_db[task_id]["status"] = status
+                if result is not None:
+                    self.tasks_db[task_id]["result"] = result
 
     def _worker(self):
         """workerのループ"""
@@ -28,7 +30,7 @@ class TaskManager:
                 result = func(*args)
                 self._update_status(task_id, "completed", result)
             except Exception as e:
-                self._update_status(task_id, "failed", error=error)
+                self._update_status(task_id, "failed", result=str(e))
 
     def _worker_start(self):
         """workerの開始"""
@@ -42,18 +44,22 @@ class TaskManager:
 
     def add_task(self, func, *args):
         """taskの追加"""
-        task_id = str(uuid.uuid4())
-        self.tasks_db[task_id] = {"status": "pending", "result": None}
-        while len(self.tasks_db) > self.max_task:
-            self.tasks_db.popitem(last=False)
+        with self.lock:  # ロックを取得
+            while len(self.tasks_db) >= self.max_task:
+                self.tasks_db.popitem(last=False)
+            task_id = str(uuid.uuid4())
+            self.tasks_db[task_id] = {"status": "pending", "result": None}
+        
         self.tasks_queue.put((func, args, task_id))
         return task_id
 
     def get_status(self, task_id):
         """状態の取得"""
-        return self.tasks_db.get(task_id)
+        with self.lock:
+            return self.tasks_db.get(task_id)
 
     def get_all_tasks(self):
         """全状態の取得"""
-        return {k: v["status"] for k, v in self.tasks_db.items()}
+        with self.lock:
+            return {k: v["status"] for k, v in self.tasks_db.items()}
 
